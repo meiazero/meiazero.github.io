200eeee9d916e68a5d9a9a56b5d7c89572759df5
Emanuel Avila <76269418+meiazero@users.noreply.github.com>
2023-01-28 19:11:34 -0300

    fix: error export _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
 
-export function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
