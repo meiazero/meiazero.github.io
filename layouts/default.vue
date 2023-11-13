8b71ba66de5623c449b7de4d3d6863ec8a8ffdcf
Emanuel Avila <avilaemanueel@gmail.com>
2023-11-12 23:01:02 -0300

    style: update default layout to use minimum screen width and height

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@ import Header from '@/components/Header.vue'
 </script>
 
 <template>
-  <div class="bg-slate-100 w-screen h-screen">
+  <div class="bg-slate-100 min-w-screen min-h-screen">
     <Header />
     <slot />
   </div>
